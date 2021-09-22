cfe266902084e7cfc2ef25b0b0fb99741a996fda
Mateus JP <mateus_jp@outlook.com>
2021-09-21 23:47:00 -0300

    criando a classe Grid

--- simClasses.py
+++ simClasses.py
@@ -1,4 +1,5 @@
-from numpy import arctan2,pi,sqrt,cos,sin,array,matmul,amin,where,zeros,delete,append,int32
+from numpy import arctan2,pi,sqrt,cos,sin,array,matmul,amin,where,take,zeros,delete,append,int32,argmin
+from scipy.spatial import distance
 
 #! Units: cm, rad, s
 
@@ -156,3 +157,87 @@ class Robot:
     #% This method print a little log on console
     def showInfo(self):
         print('xPos: {:.2f} | yPos: {:.2f} | theta: {:.2f} | velocity: {:.2f}'.format(self.xPos,self.yPos,float(self.theta),float(self.v)))
+
+class Grid:
+    def __init__(self):
+        
+        # criando um grid 5x6
+        self.gridv = array([[17.5, 13],[42.5, 13], [67.5, 13],[92.5, 13], [117.5, 13],[142.5, 13],
+                      [17.5, 39],[42.5, 39], [67.5, 39],[92.5, 39], [117.5, 39],[142.5, 39]
+                      [17.5, 65],[42.5, 65], [67.5, 65],[92.5, 65], [117.5, 65],[142.5, 65]
+                      [17.5, 91],[42.5, 91], [67.5, 91],[92.5, 91], [117.5, 91],[142.5, 91]
+                      [17.5, 117],[42.5, 117], [67.5, 117],[92.5, 117], [117.5, 117],[142.5, 117] ])
+        
+        # definindo os angulos de cada grid
+        self.AttitudeGrid = array([-pi/2, 0.47282204, 0.56910571, 0.70991061, 0.9279823, 1.27818735,
+                             -pi/2, 0.29463669, 0.35945951, 0.46006287, 0.63557154, 1.0038244,
+                             0.06148337, 0.07225452, 0.08759046, 0.11115443, 0.15188589, 0.23793116,
+                             pi/2, -0.29463669, -0.35945951, -0.46006287, -0.63557154, -1.0038244,
+                             pi/2,  -0.47282204, -0.56910571, -0.70991061, -0.9279823, -1.27818735])
+        self.robotGridPos = zeros(3)
+        self.ballGridPos = 0
+
+    def update(self, robot0, robot1, robot2, ball):
+        
+        # encontrando o indice em que cada robo e a bola se encontra
+        index0 = argmin(distance.cdist(self.gridv, [robot0.xPos, robot0.yPos], 'euclidian'))
+        index1 = argmin(distance.cdist(self.gridv, [robot1.xPos, robot1.yPos], 'euclidian'))
+        index2 = argmin(distance.cdist(self.gridv, [robot2.xPos, robot2.yPos], 'euclidian'))
+        indexb = argmin(distance.cdist(self.gridv, [ball.xPos, ball.yPos], 'euclidian'))
+
+        # Atualizando os valores
+        self.robotGridPos = array([index0, index1, index2])
+        self.ballGridPos = indexb
+
+    def bestGridMov():
+
+        # Posição dos robôs
+        pos0 = self.gridv[index[0]]
+        pos1 = self.gridv[index[1]]
+        pos2 = self.gridv[index[2]]
+
+        # Lista dos grids mais próximos de cada robô 
+        listAux0 = distance.cdist(self.gridv, self.gridv[pos0], 'euclidian') # calcula a distancia
+        listId0 = where(list0Aux == list0Aux.min()) # encontra o indice dos valores min
+        list0 = take(self.gridv, listId0) # salva a posição dos valores min
+        
+        listAux1 = distance.cdist(self.gridv, self.gridv[pos1], 'euclidian')
+        listId1 = where(listAux1 == listAux1.min())
+        list1 = take(self.gridv, listId1)
+        
+        listAux2 = distance.cdist(self.gridv, self.gridv[pos2], 'euclidian')
+        listId2 = where(listAux2 == listAux2.min())
+        list2 = take(self.gridv, listId2)
+
+        #Verifica se a posição que ele vai se mover já tem algum robô
+        
+        if self.robotGridPos[1] in listId0:
+            listId0 = delete(listId0, where(listId0 == self.robotGridPos[1]))
+            list0 = delete(list0, where(listId0 == self.robotGridPos[1]))
+        if self.robotGridPos[2] in listId0:
+            listId0 = delete(listId0, where(listId0 == self.robotGridPos[2]))
+            list0 = delete(list0, where(listId0 == self.robotGridPos[2]))
+       
+        if self.robotGridPos[0] in listId1:
+            listId1 = delete(listId1, where(listId1 == self.robotGridPos[0]))
+            list1 = delete(list1, where(listId1 == self.robotGridPos[0]))
+        if self.robotGridPos[2] in listId1:
+            listId1 = delete(listId1, where(listId1 == self.robotGridPos[2]))
+            list1 = delete(list1, where(listId1 == self.robotGridPos[2]))
+
+        if self.robotGridPos[0] in listId2:
+            listId2 = delete(listId2, where(listId2 == self.robotGridPos[0]))
+            list2 = delete(list2, where(listId2 == self.robotGridPos[0]))
+        if self.robotGridPos[1] in listId2:
+            listId2 = delete(listId2, where(listId2 == self.robotGridPos[1]))
+            list2 = delete(list2, where(listId2 == self.robotGridPos[1]))
+
+        # Encontrando qual grid é o mais próximo da bola
+        targetId0 = argmin(distance.cdist(list0, self.gridv[indexb], 'euclidian'))
+        target0 = self.gridv[listId0[targetId0]]
+
+        targetId1 = argmin(distance.cdist(list1, self.gridv[indexb], 'euclidian'))
+        target1 = self.gridv[listId0[targetId1]]
+
+        targetId2 = argmin(distance.cdist(list2, self.gridv[indexb], 'euclidian'))
+        target2 = self.gridv[listId0[targetId2]]
