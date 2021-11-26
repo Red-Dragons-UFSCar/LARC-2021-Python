09e3aabe728f6d4e1ecdd27cf0dc44f142ea4029
Pedro Lealdini <58371590+Kommnenos@users.noreply.github.com>
2021-11-26 16:33:50 -0300

    Conversao para PEP-8 1.0
    Tudo, exceto uns problemas que eu não consigo ter certeza se mudar n vão explodir nada.

--- corners.py
+++ corners.py
@@ -1,173 +1,174 @@
-from numpy import cos,sin,arctan2,sqrt,sign,pi,delete,append,array,angle, deg2rad
+from numpy import arctan2, sqrt, pi, deg2rad
+
+
 # Estas funções são utilizadas para alterar a execução das estratégias do jogador nos cantos
 # Afim de impedir que ele fique travado
 
-def targetInCorner(target, robot):
-
+def target_in_corner(target, robot):
     corner = 0
-    flagCorner = False
+    flag_corner = False
     if not robot.teamYellow:
         if target.xPos < 20:
 
-            flagCorner = True
+            flag_corner = True
             corner = 1
             if target.xPos < 5:
-                target.update(target.xPos+3, target.yPos, target.theta)
+                target.update(target.xPos + 3, target.yPos, target.theta)
             else:
-                target.update(target.xPos+1.5, target.yPos, target.theta)
+                target.update(target.xPos + 1.5, target.yPos, target.theta)
         elif target.xPos > 150:
 
-            flagCorner = True
+            flag_corner = True
             corner = 3
             if target.xPos > 155:
-                target.update(target.xPos-3, target.yPos, target.theta)
+                target.update(target.xPos - 3, target.yPos, target.theta)
             else:
-                target.update(target.xPos-1.5, target.yPos, target.theta)
+                target.update(target.xPos - 1.5, target.yPos, target.theta)
         if target.yPos < 10:
 
-            flagCorner = True
+            flag_corner = True
             corner = 2
             if target.yPos < 5:
-                target.update(target.xPos,target.yPos+3, target.theta)
+                target.update(target.xPos, target.yPos + 3, target.theta)
             else:
-                target.update(target.xPos,target.yPos+1.5, target.theta)
+                target.update(target.xPos, target.yPos + 1.5, target.theta)
         elif target.yPos > 120:
 
-            flagCorner = True
+            flag_corner = True
             corner = 4
             if target.yPos > 125:
-                target.update(target.xPos,target.yPos-3, target.theta)
+                target.update(target.xPos, target.yPos - 3, target.theta)
             else:
-                target.update(target.xPos,target.yPos-1.5, target.theta)
+                target.update(target.xPos, target.yPos - 1.5, target.theta)
     else:
         if target.xPos < 20:
 
-            flagCorner = True
+            flag_corner = True
             corner = 1
             if target.xPos < 15:
-                target.update(target.xPos+3, target.yPos, target.theta)
+                target.update(target.xPos + 3, target.yPos, target.theta)
             else:
-                target.update(target.xPos+1.5, target.yPos, target.theta)
+                target.update(target.xPos + 1.5, target.yPos, target.theta)
         elif target.xPos > 150:
 
-            flagCorner = True
+            flag_corner = True
             corner = 3
             if target.xPos > 155:
-                target.update(target.xPos-3, target.yPos, target.theta)
+                target.update(target.xPos - 3, target.yPos, target.theta)
             else:
-                target.update(target.xPos-1.5, target.yPos, target.theta)
+                target.update(target.xPos - 1.5, target.yPos, target.theta)
         if target.yPos < 10:
 
-            flagCorner = True
+            flag_corner = True
             corner = 2
             if target.yPos < 5:
-                target.update(target.xPos,target.yPos+3, target.theta)
+                target.update(target.xPos, target.yPos + 3, target.theta)
             else:
-                target.update(target.xPos,target.yPos+1.5, target.theta)
+                target.update(target.xPos, target.yPos + 1.5, target.theta)
         elif target.yPos > 120:
 
-            flagCorner = True
+            flag_corner = True
             corner = 4
             if target.yPos > 125:
-                target.update(target.xPos,target.yPos-3, target.theta)
+                target.update(target.xPos, target.yPos - 3, target.theta)
             else:
-                target.update(target.xPos,target.yPos-1.5, target.theta)
+                target.update(target.xPos, target.yPos - 1.5, target.theta)
 
     robot.spin = False
-    if flagCorner:
+    if flag_corner:
         robot.spin = True
-        changeTargetTheta(robot, target,corner)
+        change_target_theta(robot, target, corner)
 
-    return flagCorner, corner
+    return flag_corner, corner
 
-def changeTargetTheta(robot, target,corner):
 
-    dist = sqrt((robot.xPos- target.xPos)**2 + (robot.yPos- target.yPos)**2)
+def change_target_theta(robot, target, corner):
+    dist = sqrt((robot.xPos - target.xPos) ** 2 + (robot.yPos - target.yPos) ** 2)
 
     if not robot.teamYellow:
-        if (corner == 2 or corner == 4):
+        if corner == 2 or corner == 4:
             if dist < 6:
                 if robot.yPos < 75:
-                    thetaGol = arctan2(75, 160- robot.xPos)
+                    theta_gol = arctan2(75, 160 - robot.xPos)
 
                 else:
-                    thetaGol = arctan2(-75, 160- robot.xPos)
-                target.update(target.xPos,target.yPos,thetaGol)
+                    theta_gol = arctan2(-75, 160 - robot.xPos)
+                target.update(target.xPos, target.yPos, theta_gol)
             else:
-                target.update(target.xPos,target.yPos,0)
+                target.update(target.xPos, target.yPos, 0)
 
         elif robot.yPos > 110:
             if corner == 1:
-                target.update(target.xPos,target.yPos,pi/2)
+                target.update(target.xPos, target.yPos, pi / 2)
             elif corner == 3:
-                target.update(target.xPos,target.yPos,-pi/2)
+                target.update(target.xPos, target.yPos, -pi / 2)
         elif robot.yPos < 40:
             if corner == 1:
-                target.update(target.xPos,target.yPos,-pi/2)
+                target.update(target.xPos, target.yPos, -pi / 2)
             elif corner == 3:
-                target.update(target.xPos,target.yPos,pi/2)
+                target.update(target.xPos, target.yPos, pi / 2)
     else:
-        if (corner == 2 or corner == 4):
+        if corner == 2 or corner == 4:
             if dist < 6:
                 if robot.yPos < 75:
-                    thetaGol = arctan2(75, 10- robot.xPos)
+                    theta_gol = arctan2(75, 10 - robot.xPos)
 
                 else:
-                    thetaGol = arctan2(-75, 10- robot.xPos)
-                target.update(target.xPos,target.yPos,thetaGol)
+                    theta_gol = arctan2(-75, 10 - robot.xPos)
+                target.update(target.xPos, target.yPos, theta_gol)
             else:
                 if target.yPos > 65:
-                    target.update(target.xPos,target.yPos,-pi+deg2rad(10))
+                    target.update(target.xPos, target.yPos, -pi + deg2rad(10))
                 else:
-                    target.update(target.xPos,target.yPos, pi-deg2rad(10))
+                    target.update(target.xPos, target.yPos, pi - deg2rad(10))
 
         elif robot.yPos > 110:
             if corner == 1:
-                target.update(target.xPos,target.yPos,-pi/2)
+                target.update(target.xPos, target.yPos, -pi / 2)
             elif corner == 3:
-                target.update(target.xPos,target.yPos,pi/2)
+                target.update(target.xPos, target.yPos, pi / 2)
         elif robot.yPos < 40:
             if corner == 1:
-                target.update(target.xPos,target.yPos,pi/2)
+                target.update(target.xPos, target.yPos, pi / 2)
             elif corner == 3:
-                target.update(target.xPos,target.yPos,-pi/2)
+                target.update(target.xPos, target.yPos, -pi / 2)
 
     return None
 
-def robotLockedCorner(target, robot):
 
+def robot_locked_corner(target, robot):
     corner = 0
-    flagLocked = False
-    if (robot.xPos < 3 and (robot.yPos > 110 or robot.yPos < 40)):
-        if (abs(robot.theta) < 0.35 or abs(robot.theta - pi) < 0.35):
-            flagLocked = True
+    flag_locked = False
+    if robot.xPos < 3 and (robot.yPos > 110 or robot.yPos < 40):
+        if abs(robot.theta) < 0.35 or abs(robot.theta - pi) < 0.35:
+            flag_locked = True
             corner = 1
-    elif (robot.xPos > 147 and (robot.yPos > 110 or robot.yPos < 40)):
-        if (abs(robot.theta) < 0.35 or abs(robot.theta - pi) < 0.35):
-            flagLocked = True
+    elif robot.xPos > 147 and (robot.yPos > 110 or robot.yPos < 40):
+        if abs(robot.theta) < 0.35 or abs(robot.theta - pi) < 0.35:
+            flag_locked = True
             corner = 3
     if robot.yPos < 5:
-        if ((abs(robot.theta) < ((pi/2)+0.35)) and (abs(robot.theta) > ((pi/2)-0.35))):
-            flagLocked = True
+        if (abs(robot.theta) < ((pi / 2) + 0.35)) and (abs(robot.theta) > ((pi / 2) - 0.35)):
+            flag_locked = True
             corner = 2
     elif robot.yPos > 125:
-        if ((abs(robot.theta) < ((pi/2)+0.35)) and (abs(robot.theta) < ((pi/2)-0.35))):
-            flagLocked = True
+        if (abs(robot.theta) < ((pi / 2) + 0.35)) and (abs(robot.theta) < ((pi / 2) - 0.35)):
+            flag_locked = True
             corner = 4
 
-    if flagLocked:
-        changeTargetPos(robot, target,corner)
+    if flag_locked:
+        change_target_pos(robot, target, corner)
 
-    return flagLocked, corner
+    return flag_locked, corner
 
-def changeTargetPos(robot, target,corner):
 
+def change_target_pos(robot, target, corner):
     if corner == 1:
-        target.update(robot.xPos+100,robot.yPos,0)
+        target.update(robot.xPos + 100, robot.yPos, 0)
     if corner == 2:
-        target.update(robot.xPos,robot.yPos+100,pi/2)
+        target.update(robot.xPos, robot.yPos + 100, pi / 2)
     if corner == 3:
-        target.update(robot.xPos-100,robot.yPos,0)
+        target.update(robot.xPos - 100, robot.yPos, 0)
     if corner == 4:
-        target.update(robot.xPos,robot.yPos-100,-pi/2)
+        target.update(robot.xPos, robot.yPos - 100, -pi / 2)
     return None
--- simClasses.py
+++ simClasses.py
@@ -1,106 +1,109 @@
-from numpy import arctan2,pi,sqrt,cos,sin,array,matmul,amin,where,take,zeros,delete,append,int32,argmin
+from numpy import sqrt, array, amin, where, zeros, delete, append, int32, argmin
+
+
 # from scipy.spatial import distance -> Descomentar quando atividade do Grid voltar
 
-#! Units: cm, rad, s
+# ! Units: cm, rad, s
 
-#% Class to set the targets of each robot in game
+# % Class to set the targets of each robot in game
 class Target:
     def __init__(self):
-        self.xPos=0         #? Desired x position
-        self.yPos=0         #? Desired y position
-        self.theta=0        #? Orientation at the desired point (x,y)
+        self.xPos = 0  # ? Desired x position
+        self.yPos = 0  # ? Desired y position
+        self.theta = 0  # ? Orientation at the desired point (x,y)
 
-    #% Setter
-    def update(self,x,y,theta):
-        self.xPos=x
-        self.yPos=y
-        self.theta=theta
+    # % Setter
+    def update(self, x, y, theta):
+        self.xPos = x
+        self.yPos = y
+        self.theta = theta
 
-    #% This method print a little log on console
-    def showInfo(self):
-        print('xPos: {:.2f} | yPos: {:.2f} | theta: {:.2f}'.format(self.xPos,self.yPos,float(self.theta)))
+    # % This method print a little log on console
+    def show_info(self):
+        print('xPos: {:.2f} | yPos: {:.2f} | theta: {:.2f}'.format(self.xPos, self.yPos, float(self.theta)))
 
-#% Class to set the obstacle of each robot
+
+# % Class to set the obstacle of each robot
 class Obstacle:
     def __init__(self):
-        self.xPos=0         #? Obstacle x position
-        self.yPos=0         #? Obstacle y position
-        self.v=0            #? Obstacle velocity (cm/s)
-        self.theta=0        #? Obstacle orientation
+        self.xPos = 0  # ? Obstacle x position
+        self.yPos = 0  # ? Obstacle y position
+        self.v = 0  # ? Obstacle velocity (cm/s)
+        self.theta = 0  # ? Obstacle orientation
 
-    #% Setter
-    def setObst(self,x,y,v,theta):
-        self.xPos=x
-        self.yPos=y
-        self.v=v
-        self.theta=theta
+    # % Setter
+    def set_obst(self, x, y, v, theta):
+        self.xPos = x
+        self.yPos = y
+        self.v = v
+        self.theta = theta
 
-    #% This method verify which is the closest obstacle and sets it as the current obstacle to avoid
-    def update(self,robot,friend1,friend2,enemy1=None,enemy2=None,enemy3=None):
+    # % This method verify which is the closest obstacle and sets it as the current obstacle to avoid
+    def update(self, robot, friend1, friend2, enemy1=None, enemy2=None, enemy3=None):
         if (enemy1 is None) and (enemy2 is None) and (enemy3 is None):
-            d=array([[robot.dist(friend1)],
-                     [robot.dist(friend2)]])
+            d = array([[robot.dist(friend1)],
+                       [robot.dist(friend2)]])
         elif (enemy2 is None) and (enemy3 is None):
-            d=array([[robot.dist(friend1)],
-                     [robot.dist(friend2)],
-                     [robot.dist(enemy1) ]])
-        elif (enemy3 is None):
-            d=array([[robot.dist(friend1)],
-                     [robot.dist(friend2)],
-                     [robot.dist(enemy1) ],
-                     [robot.dist(enemy2) ]])
+            d = array([[robot.dist(friend1)],
+                       [robot.dist(friend2)],
+                       [robot.dist(enemy1)]])
+        elif enemy3 is None:
+            d = array([[robot.dist(friend1)],
+                       [robot.dist(friend2)],
+                       [robot.dist(enemy1)],
+                       [robot.dist(enemy2)]])
         else:
-            d=array([[robot.dist(friend1)],
-                     [robot.dist(friend2)],
-                     [robot.dist(enemy1) ],
-                     [robot.dist(enemy2) ],
-                     [robot.dist(enemy3) ]])
+            d = array([[robot.dist(friend1)],
+                       [robot.dist(friend2)],
+                       [robot.dist(enemy1)],
+                       [robot.dist(enemy2)],
+                       [robot.dist(enemy3)]])
 
-        index=where(d==amin(d))
-        if index[0][0]==0:
-            self.setObst(friend1.xPos,friend1.yPos,friend1.v,friend1.theta)
-        elif index[0][0]==1:
-            self.setObst(friend2.xPos,friend2.yPos,friend2.v,friend2.theta)
-        elif index[0][0]==2:
-            self.setObst(enemy1.xPos,enemy1.yPos,0,0)
-        elif index[0][0]==3:
-            self.setObst(enemy2.xPos,enemy2.yPos,0,0)
+        index = where(d == amin(d))
+        if index[0][0] == 0:
+            self.set_obst(friend1.xPos, friend1.yPos, friend1.v, friend1.theta)
+        elif index[0][0] == 1:
+            self.set_obst(friend2.xPos, friend2.yPos, friend2.v, friend2.theta)
+        elif index[0][0] == 2:
+            self.set_obst(enemy1.xPos, enemy1.yPos, 0, 0)
+        elif index[0][0] == 3:
+            self.set_obst(enemy2.xPos, enemy2.yPos, 0, 0)
         else:
-            self.setObst(enemy3.xPos,enemy3.yPos,0,0)
+            self.set_obst(enemy3.xPos, enemy3.yPos, 0, 0)
 
-    def update2(self,robot,ball,friend1,friend2,enemy1,enemy2,enemy3):
+    def update2(self, robot, ball, friend1, friend2, enemy1, enemy2, enemy3):
         enemys = array([enemy1, enemy2, enemy3])
-        d_ball=array([[enemy1.dist(ball)],
-                      [enemy2.dist(ball)],
-                      [enemy3.dist(ball)]])
-        index=argmin(d_ball)
+        d_ball = array([[enemy1.dist(ball)],
+                        [enemy2.dist(ball)],
+                        [enemy3.dist(ball)]])
+        index = argmin(d_ball)
         if d_ball[index] < 15:
             enemys = delete(enemys, [index])
 
         if not robot.teamYellow:
-            xGol = 160
-            yGol = 65
+            x_gol = 160
+            y_gol = 65
         else:
-            xGol = 10
-            yGol = 65
+            x_gol = 10
+            y_gol = 65
 
-        if len(enemys)==3:
-            d1 = sqrt( (xGol-enemy1.xPos)**2 + (yGol-enemy1.yPos)**2 )
-            d2 = sqrt( (xGol-enemy2.xPos)**2 + (yGol-enemy2.yPos)**2 )
-            d3 = sqrt( (xGol-enemy3.xPos)**2 + (yGol-enemy3.yPos)**2 )
-            d_gol=array([[d1],
-                         [d2],
-                         [d3]])
-            index=argmin(d_gol)
-            dballgol = sqrt( (xGol-ball.xPos)**2 + (yGol-ball.yPos)**2 )
+        if len(enemys) == 3:
+            d1 = sqrt((x_gol - enemy1.xPos) ** 2 + (y_gol - enemy1.yPos) ** 2)
+            d2 = sqrt((x_gol - enemy2.xPos) ** 2 + (y_gol - enemy2.yPos) ** 2)
+            d3 = sqrt((x_gol - enemy3.xPos) ** 2 + (y_gol - enemy3.yPos) ** 2)
+            d_gol = array([[d1],
+                           [d2],
+                           [d3]])
+            index = argmin(d_gol)
+            dballgol = sqrt((x_gol - ball.xPos) ** 2 + (y_gol - ball.yPos) ** 2)
             if d_gol[index] < 20 and dballgol < 20:
                 enemys = delete(enemys, index)
         else:
-            d1 = sqrt( (xGol-enemys[0].xPos)**2 + (yGol-enemys[0].yPos)**2 )
-            d2 = sqrt( (xGol-enemys[1].xPos)**2 + (yGol-enemys[1].yPos)**2 )
-            d_gol=array([[d1],
-                         [d2]])
-            index=argmin(d_gol)
+            d1 = sqrt((x_gol - enemys[0].xPos) ** 2 + (y_gol - enemys[0].yPos) ** 2)
+            d2 = sqrt((x_gol - enemys[1].xPos) ** 2 + (y_gol - enemys[1].yPos) ** 2)
+            d_gol = array([[d1],
+                           [d2]])
+            index = argmin(d_gol)
             if d_gol[index] < 20:
                 enemys = delete(enemys, index)
 
@@ -113,34 +116,35 @@ class Obstacle:
         # for i in range(len(enemys)):
         #     print("Index: ", enemys[i].index)
         for i in range(len(enemys)):
-           d_robot[i] = robot.dist(enemys[i])
+            d_robot[i] = robot.dist(enemys[i])
 
-        index=argmin(d_robot)
-        self.setObst(enemys[index].xPos,enemys[index].yPos,0,0)
+        index = argmin(d_robot)
+        self.set_obst(enemys[index].xPos, enemys[index].yPos, 0, 0)
         # if enemys[index].teamYellow:
         #     print("Obstaculo: Amarelo " + str(enemys[index].index))
         # else:
         #     print("Obstaculo: Azul " + str(enemys[index].index))
-        self.setObst(enemys[index].xPos,enemys[index].yPos,0,0)
+        self.set_obst(enemys[index].xPos, enemys[index].yPos, 0, 0)
+
+    # % This method print a little log on console
+    def show_info(self):
+        print('xPos: {:.2f} | yPos: {:.2f} | theta: {:.2f} | velocity: {:.2f}'.format(self.xPos, self.yPos,
+                                                                                      float(self.theta), self.v))
 
 
-    #% This method print a little log on console
-    def showInfo(self):
-        print('xPos: {:.2f} | yPos: {:.2f} | theta: {:.2f} | velocity: {:.2f}'.format(self.xPos,self.yPos,float(self.theta),self.v))
-
-#% Class to create the ball in game
+# % Class to create the ball in game
 class Ball:
     def __init__(self):
-        self.xPos=0
-        self.yPos=0
-        self.vx=0
-        self.vy=0
-        self.pastPose=zeros(4).reshape(2,2) #? Stores the last 3 positions (x,y) => updated on self.simGetPose()
+        self.xPos = 0
+        self.yPos = 0
+        self.vx = 0
+        self.vy = 0
+        self.pastPose = zeros(4).reshape(2, 2)  # ? Stores the last 3 positions (x,y) => updated on self.simGetPose()
 
-    #% This method gets position of the ball in FIRASim
-    def simGetPose(self, data_ball):
-        self.xPos = data_ball.x + data_ball.vx*100*12/60
-        self.yPos = data_ball.y + data_ball.vy*100*12/60
+    # % This method gets position of the ball in FIRASim
+    def sim_get_pose(self, data_ball):
+        self.xPos = data_ball.x + data_ball.vx * 100 * 12 / 60
+        self.yPos = data_ball.y + data_ball.vy * 100 * 12 / 60
 
         # check if prev is out of field, in this case reflect ball moviment to reproduce the collision
         if self.xPos > 160:
@@ -151,86 +155,88 @@ class Ball:
         if self.yPos > 130:
             self.yPos = 130 - (self.yPos - 130)
         elif self.yPos < 0:
-            self.yPos = - (self.yPos)
+            self.yPos = - self.yPos
 
         self.vx = data_ball.vx
         self.vy = data_ball.vy
 
+    # % This method print a little log on console
+    def show_info(self):
+        print('xPos: {:.2f} | yPos: {:.2f}'.format(self.xPos, self.yPos))
 
-    #% This method print a little log on console
-    def showInfo(self):
-        print('xPos: {:.2f} | yPos: {:.2f}'.format(self.xPos,self.yPos))
 
-#% Class to create the robots in game
+# % Class to create the robots in game
 class Robot:
     def __init__(self, index, actuator, mray):
-        self.flagDirectGoal=False
-        self.flagCruzamento=False
-        self.flagTrocaFace=False
-        self.teamYellow=mray
-        self.spin=False
-        self.contStopped=0
-        self.index=int32(index)
-        self.actuator=actuator
-        self.face=1                          #? Defines the current face of the robot
-        self.xPos=0                          #? X position
-        self.yPos=0                          #? Y position
-        self.theta=0                         #? Orientation
-        self.rightMotor=0                    #? Right motor handle
-        self.leftMotor=0                     #? Left motor handle
-        self.v=0                             #? Velocity (cm/s) => updated on execution.py
-        self.vx=0
-        self.vy=0
-        self.vTheta=0
-        self.vL=0                            #? Left wheel velocity (cm/s) => updated on simClasses.py -> simSetVel()
-        self.vR=0                            #? Right wheel velocity (cm/s) =>  updated on simClasses.py -> simSetVel()
-        self.vMax=35                             #! Robot max velocity (cm/s)
-        self.rMax=3*self.vMax                #! Robot max rotation velocity (rad*cm/s)
-        self.L=7.5                           #? Base length of the robot (cm)
-        self.LSimulador=6.11                 #? Base length of the robot on coppelia (cm)
-        self.R=3.4                           #? Wheel radius (cm)
-        self.obst=Obstacle()                 #? Defines the robot obstacle
-        self.target=Target()                 #? Defines the robot target
-        self.pastPose=zeros(12).reshape(4,3) #? Stores the last 3 positions (x,y) and orientation => updated on execution.py
+        self.flagDirectGoal = False
+        self.flagCruzamento = False
+        self.flagTrocaFace = False
+        self.teamYellow = mray
+        self.spin = False
+        self.contStopped = 0
+        self.index = int32(index)
+        self.actuator = actuator
+        self.face = 1  # ? Defines the current face of the robot
+        self.xPos = 0  # ? X position
+        self.yPos = 0  # ? Y position
+        self.theta = 0  # ? Orientation
+        self.rightMotor = 0  # ? Right motor handle
+        self.leftMotor = 0  # ? Left motor handle
+        self.v = 0  # ? Velocity (cm/s) => updated on execution.py
+        self.vx = 0
+        self.vy = 0
+        self.vTheta = 0
+        self.vL = 0  # ? Left wheel velocity (cm/s) => updated on simClasses.py -> simSetVel()
+        self.vR = 0  # ? Right wheel velocity (cm/s) =>  updated on simClasses.py -> simSetVel()
+        self.vMax = 35  # ! Robot max velocity (cm/s)
+        self.rMax = 3 * self.vMax  # ! Robot max rotation velocity (rad*cm/s)
+        self.L = 7.5  # ? Base length of the robot (cm)
+        self.LSimulador = 6.11  # ? Base length of the robot on coppelia (cm)
+        self.R = 3.4  # ? Wheel radius (cm)
+        self.obst = Obstacle()  # ? Defines the robot obstacle
+        self.target = Target()  # ? Defines the robot target
+        # ? Stores the last 3 positions (x,y) and orientation => updated on execution.py
+        self.pastPose = zeros(12).reshape(4,
+                                          3)
 
+    # % This method calculates the distance between the robot and an object
+    def dist(self, obj):
+        return sqrt((self.xPos - obj.xPos) ** 2 + (self.yPos - obj.yPos) ** 2)
 
-
-    #% This method calculates the distance between the robot and an object
-    def dist(self,obj):
-        return sqrt((self.xPos-obj.xPos)**2+(self.yPos-obj.yPos)**2)
-
-    #% This method returns True if the distance between the target and the robot is less than 5cm - False otherwise
+    # % This method returns True if the distance between the target and the robot is less than 5cm - False otherwise
     def arrive(self):
-        if self.dist(self.target)<=5:
+        if self.dist(self.target) <= 5:
             return True
         else:
             return False
 
-    #% This method gets both position and orientation of the robot in FIRASim
-    def simGetPose(self, data_robot):
+    # % This method gets both position and orientation of the robot in FIRASim
+    def sim_get_pose(self, data_robot):
         self.xPos = data_robot.x
         self.yPos = data_robot.y
         self.vx = data_robot.vx
         self.vy = data_robot.vy
         self.theta = data_robot.a
         self.vTheta = data_robot.va
-        self.v = sqrt(self.vx**2 + self.vy**2)
+        self.v = sqrt(self.vx ** 2 + self.vy ** 2)
 
-    def simSetVel(self,v,w):
-        if self.face==1:
-            self.vR=v+0.5*self.L*w
-            self.vL=v-0.5*self.L*w
+    def sim_set_vel(self, v, w):
+        if self.face == 1:
+            self.vR = v + 0.5 * self.L * w
+            self.vL = v - 0.5 * self.L * w
         else:
-            self.vL=-v-0.5*self.L*w
-            self.vR=-v+0.5*self.L*w
+            self.vL = -v - 0.5 * self.L * w
+            self.vR = -v + 0.5 * self.L * w
         self.actuator.send(self.index, self.vL, self.vR)
 
-    def simSetVel2(self,v1,v2):
+    def sim_set_vel2(self, v1, v2):
         self.actuator.send(self.index, v1, v2)
 
-    #% This method print a little log on console
-    def showInfo(self):
-        print('xPos: {:.2f} | yPos: {:.2f} | theta: {:.2f} | velocity: {:.2f}'.format(self.xPos,self.yPos,float(self.theta),float(self.v)))
+    # % This method print a little log on console
+    def show_info(self):
+        print('xPos: {:.2f} | yPos: {:.2f} | theta: {:.2f} | velocity: {:.2f}'.format(self.xPos, self.yPos,
+                                                                                      float(self.theta), float(self.v)))
+
 
 '''
 -------------------Descomentar quando atividade do Grid voltar
