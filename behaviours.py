7113d8012ba55f6f21e592dfb8888d73a3ae1f0e
IvanCalvo <ivandc1998.ic@gmail.com>
2023-07-26 00:32:06 -0300

    update behaviours constants

--- behaviours.py
+++ behaviours.py
@@ -6,8 +6,8 @@ class Univector:
 
     def __init__(self):
         #? Constants learned from EP
-        self.d_e=6                                      # Constante relacionada ao tamanho das espirais
-        self.k_r=3                                    # Constante de suavização do campo
+        self.d_e=11  # 6 -> 11                                    # Constante relacionada ao tamanho das espirais
+        self.k_r=6   # 3-> 6                                # Constante de suavização do campo
         self.delta=5                                  # Variancia da gaussiana de obstaculo
         self.k_o=0.5                                   # Constante de proporcionalidade velocidade do obstaculo
         self.d_min=5 #* => modified: EP = 3.48          # Distancia mínima em que o campo se torna puro repulsivo
